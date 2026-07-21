223277e67b3c8e7980df34bbbf4a625ae0b6ec65
mudimurtala <ibnmuhyideen95@gmail.com>
2026-07-21 14:55:48 +0100

    Testing a web-analytics tool(Sabilytics)

--- src/App.tsx
+++ src/App.tsx
@@ -48,3 +48,19 @@ export default function App() {
     </Router>
   );
 }
+
+
+import { useEffect } from "react";
+
+export function Sabilytics() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://sabilytics.vercel.app/script.js";
+    script.setAttribute("data-site", "8sj835pw3yxz");
+    script.setAttribute("data-domain", "innohealth.tech");
+    document.head.appendChild(script);
+  }, []);
+
+  return null;
+}
